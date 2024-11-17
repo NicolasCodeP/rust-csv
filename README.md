8a33980bd32f00ae09f11c841925790918c2637c
blueniarb <blueniarb@gmail.com>
2024-11-17 17:11:54 +0800

    csv_reader

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Rust Boilerplate
+# CSV Reader
 
-## Overview
+## Description
 
-This is a boilerplate for Rust projects. It includes a `Makefile` for building and testing the project, and a `.gitignore` file for ignoring common Rust artifacts.
+This is a simple CSV reader that reads a CSV file and print out the data.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::error::Error;
+
+use csv;
+
+fn read_from_file(file_path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(file_path)?;
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = read_from_file("./customers.csv") {
+        eprintln!("{}", e);
+    }
 }
